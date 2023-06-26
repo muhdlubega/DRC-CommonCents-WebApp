51afbe82a887c23eaae4d3f3066eca2670d088d9
Vinothinni <vinothinni@besquare.com>
2023-06-26 08:25:03 +0800

    Fixed the footer alignment,add pictures and finish about us page

--- src/components/homepage/Footer.tsx
+++ src/components/homepage/Footer.tsx
@@ -1,64 +1,9 @@
-
-
 import React from 'react';
 import '../../styles/main.scss';
-
-// const Footer: React.FC = () => {
-//     const handleScrollToTop = () => {
-//         window.scrollTo({ top: 0, behavior: 'smooth' });
-//     };
-//     const playStoreBtn = ()=>{
-      
-//     };
-//     return (
-//         <div className="footer">
-//             <div className="scroll-to-top" onClick={handleScrollToTop}>
-//                 <i className="fas fa-arrow-up"></i> 
-//             </div>
-//             <div className="container">
-//                 <div className="row">
-//                     <div className="footer-column">
-//                         <button className="playstore-btn" onClick={playStoreBtn }>
-//                             Play Store For Download
-//                         </button>
-//                     </div>
-//                     <div className="footer-column">
-//                           <h4>Useful Links</h4>
-//                           <ul>
-//                             <li className="nav-item">
-//                                 <a className="">hfiuwefj</a>
-//                             </li>
-//                             <li className="nav-item">
-//                                 <a className="">nfrfir</a>
-//                             </li>
-//                             <li className="nav-item">
-//                                 <a className="">dnfoijrfi</a>
-//                             </li>
-//                           </ul>
-//                     </div>
-//                     <div className="footer-column">
-//                         <h4>Contact Us</h4>
-//                         <p><i className="fa-solid fa-phone-volume"></i> 0112345678</p>
-//                         <p><i className="fa-solid fa-envelope"></i>commoncents@gmail.com</p>
-//                      </div>
-//                      <div className="col-md-6 col-lg-2 col-12 ft-4">
-//                        <div className="footer-icons">
-//                            <h4>Social Media</h4>
-//                             <i className="fa-brands fa-facebook-square">Facebook</i>
-//                             <i className="fa-brands fa-instagram-square">Instagram</i>
-//                             <i className="fa-brands fa-twitter-square">Twitter</i>
-//                         </div>
-//                      </div>
-//                </div>
-//             </div>
-//             <div className="footer-bottom">
-//              <p>Copyright&copy; 2023 CommonCents.All right reserved.</p> 
-//             </div>
-//         </div>
-         
-      
-//     );
-// }
+import Image1 from '../../assets/images/google-ps.png'
+import SocialMedia1 from '../../assets/images/facebook.png'
+import SocialMedia2 from '../../assets/images/ig.png'
+import SocialMedia3 from '../../assets/images/twitter.png'
 
 const Footer: React.FC = () => {
     const handleScrollToTop = () => {
@@ -67,13 +12,15 @@ const Footer: React.FC = () => {
 
     return (
         <div className="footer">
-            <div className="scroll-to-top" onClick={handleScrollToTop}>
-                <i className="fas fa-arrow-up"></i>
-            </div>
-            <div className="container">
+            {/* <div className="scroll-to-top" onClick={handleScrollToTop}>
+               <i className="fas fa-chevron-up"></i>
+            </div>  */}
+            <div className="footer-container">
                 <div className="row">
-                    <div className="col-md-6 col-lg-4 ft-1">
-                        <button className="btn btn-primary playstore-btn">GET IT ON GOOGLE PLAY</button>
+                    <div className="col-md-6 col-lg-3 ft-1">
+                    <a href="/">
+                        <img src={Image1} alt="Image Button"></img>
+                    </a>
                     </div>
                     
                     <div className="col-md-6 col-lg-3 ft-2">
@@ -92,12 +39,12 @@ const Footer: React.FC = () => {
                         <p><i className="fas fa-phone-volume"></i> &nbsp; 0112345678</p>
                         <p><i className="fas fa-envelope"></i> &nbsp; commoncents@gmail.com</p>
                     </div>
-                    <div className="col-md-6 col-lg-2  ft-4">
+                    <div className="col-md-6 col-lg-3 ft-4">
                         <h4>Socials</h4>
-                        <div className="footer-icons">
-                            <i className="fa-brands fa-square-facebook"></i>
-                            <i className="fa-brands fa-square-instagram"></i>
-                            <i className="fa-brands fa-square-twitter"></i>
+                        <div>
+                         <a href="/"><img src={SocialMedia1} alt="Image Button" style={{ width: '70px',marginLeft:-5,marginRight:3,height:'72px',marginTop:'11px'}}></img></a>
+                         <a href="/"><img src={SocialMedia2} alt="Image Button"></img></a>
+                         <a href="/"><img src={SocialMedia3} alt="Image Button"></img></a>
                         </div>
                     </div>
                 </div>
@@ -109,5 +56,7 @@ const Footer: React.FC = () => {
     );
 };
 
-
 export default Footer;
+
+
+
